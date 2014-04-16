80cf46121c2c3af9ac58839844fbb61bb3e3d2f6
tanzedan <tanzedan.gmail.com>
2014-04-16 02:54:11 +0100

    Site updated: 2014-04-16 02:54:10

---
+++ js/wb.js
@@ -0,0 +1,9 @@
+WB2.anyWhere(function(W){
+    W.widget.followButton({
+        'nick_name': '谭泽丹',  //用户昵称
+        'id': "wb_follow_btn",
+        'show_head' : true, //是否显示头像
+        'show_name' : true, //是否显示名称
+        'show_cancel': true //是否显示取消关注按钮
+    });
+});
